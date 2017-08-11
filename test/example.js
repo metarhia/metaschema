177a7ca3dfeabc65483d31759e3703c5adae7e03
tshemsedinov <timur.shemsedinov@gmail.com>
2017-08-11 06:20:56 +0300

    Multiline comments
    Closes: https://github.com/metarhia/metaschema/issues/13

--- test/example.js
+++ test/example.js
@@ -1,16 +1,21 @@
 'use strict';
 
 const methodName = (
-  // Method description
-  par1, // number, comment
-  par2, // array of string
-  par3 // comment
+  // Method caption
+  // First line description
+  // second line of multiline method description
+  a1, // number, comment
+  // multiline comment
+  // for first argument
+  a2, // array of string
+  a3 // comment
   // Returns: string, comment
   // Hint: message
-  // Example: methodName(5, [2, 3])
+  // Example: methodName(5, [2, 3], 'f')
+  // Result: '52,3f'
 ) => {
-  const s = par2.join();
-  return par1 + s + par3;
+  const s = a2.join();
+  return a1 + s + a3;
 };
 
 module.exports = {
