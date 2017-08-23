0df2456dfd5e77c4e3259402ae4e57b54ca65031
tshemsedinov <timur.shemsedinov@gmail.com>
2017-08-23 03:19:55 +0300

    Create factory from definition

--- test/schema.js
+++ test/schema.js
@@ -2,8 +2,26 @@
 
 const metaschema = require('..');
 
-metaschema.loadSchema('geometry', (err, schema) => {
+metaschema.load('geometry', (err, schema) => {
   if (err) throw err;
-  const geometry = metaschema.buildSchema(schema);
+  const geometry = metaschema.build(schema);
   console.dir({ geometry });
+
+  const p1 = metaschema.create('Point', { x: 3, y: 5 });
+  console.dir({ p1 });
+
+  const p2 = geometry.Point({ x: 3, y: 5 });
+  console.dir({ p2 });
+
+  const p3 = geometry.Point([3, 5]);
+  console.dir({ p3 });
+
+  const p4 = geometry.Point(3, 5);
+  console.dir({ p4 });
+
+  const p5 = geometry.Point({ x: 'hello', y: 2 });
+  console.dir({ p5 });
+
+  const p6 = geometry.Point({ name: 'Marcus' });
+  console.dir({ p6 });
 });
