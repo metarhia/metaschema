2e9c5c0800743db7d465ac401ec965409c3a552a
tshemsedinov <timur.shemsedinov@gmail.com>
2017-08-24 01:34:53 +0300

    Change category factory invocation style

--- test/schema.js
+++ test/schema.js
@@ -5,23 +5,22 @@ const metaschema = require('..');
 metaschema.load('geometry', (err, schema) => {
   if (err) throw err;
   const geometry = metaschema.build(schema);
-  console.dir({ geometry });
 
   const p1 = metaschema.create('Point', { x: 3, y: 5 });
   console.dir({ p1 });
 
-  const p2 = geometry.Point({ x: 3, y: 5 });
+  const p2 = geometry('Point', { x: 3, y: 5 });
   console.dir({ p2 });
 
-  const p3 = geometry.Point([3, 5]);
+  const p3 = geometry('Point', [3, 5]);
   console.dir({ p3 });
 
-  const p4 = geometry.Point(3, 5);
+  const p4 = geometry('Point', 3, 5);
   console.dir({ p4 });
 
-  const p5 = geometry.Point({ x: 'hello', y: 2 });
+  const p5 = geometry('Point', { x: 'hello', y: 2 });
   console.dir({ p5 });
 
-  const p6 = geometry.Point({ name: 'Marcus' });
+  const p6 = geometry('Point', { name: 'Marcus' });
   console.dir({ p6 });
 });
