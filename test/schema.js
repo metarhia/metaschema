30fa63044597ceac3ec37ed145cfb10512bc765c
tshemsedinov <timur.shemsedinov@gmail.com>
2017-08-24 03:09:18 +0300

    Implement recursive schema application
    PR-URL: https://github.com/metarhia/metaschema/pull/20

--- test/schema.js
+++ test/schema.js
@@ -23,4 +23,17 @@ metaschema.load('geometry', (err, schema) => {
 
   const p6 = geometry('Point', { name: 'Marcus' });
   console.dir({ p6 });
+
+  const l1 = geometry('Line', { x: 1, y: 3 }, { x: 2, y: 5 });
+  console.dir({ l1 });
+
+  const l2 = geometry('Line', [[1, 3], [2, 5]]);
+  console.dir({ l2 });
+
+  const l3 = geometry('Line', [1, 3], [2, 5]);
+  console.dir({ l3 });
+
+  const l4 = geometry('Line', { name: 'Marcus' }, { x: 2, y: true });
+  console.dir({ l4 });
+
 });
