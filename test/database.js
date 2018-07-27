0b459c0afda3abcd5390cb2e9bb493685cf2341d
tshemsedinov <timur.shemsedinov@gmail.com>
2018-07-27 22:49:38 +0300

    Remove enumerated type, use Enum decorator instead
    Closes: https://github.com/metarhia/metaschema/issues/49

--- test/database.js
+++ test/database.js
@@ -5,6 +5,13 @@ const metaschema = require('..');
 metaschema.load('general', (err, schema) => {
   if (err) throw err;
   metaschema.build(schema);
+
   const Person = metaschema.categories.get('Person');
   console.dir({ Person }, { depth: null });
+
+  const Language = metaschema.categories.get('Language');
+  console.dir({ Language }, { depth: null });
+
+  const FullName = metaschema.categories.get('FullName');
+  console.dir({ FullName }, { depth: null });
 });
