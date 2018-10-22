c43f24c2b58a06dcaa83223e258a705bce7d0952
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2018-10-22 21:10:02 +0300

    Add tests for schema validation
    Closes: https://github.com/metarhia/metaschema/issues/132
    PR-URL: https://github.com/metarhia/metaschema/pull/134

---
+++ test/validateSchema.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const path = require('path');
+const metatests = require('metatests');
+const metaschema = require('..');
+
+const schemaDir = name => path.join(__dirname, 'schemas', name);
+
+const test = config => metatests.test(config.name, test => {
+  const dir = schemaDir(config.schemaName);
+  metaschema.fs.loadAndCreate(dir, null, error => {
+    if (!config.error) {
+      test.error(error);
+    } else {
+      test.isError(error);
+      test.equal(error.errors.length, 1, 'must only return one error');
+      const [actual] = error.errors;
+      test.strictSame(actual.type, config.error.type);
+      test.strictSame(actual.source, config.error.source);
+      test.strictSame(actual.property, config.error.property);
+    }
+    test.end();
+  });
+});
+
+const testConfigs = [
+  {
+    name: 'Validate unresolved domain',
+    schemaName: 'unresolvedDomainValidation',
+    error: {
+      type: 'unresolvedDomain',
+      source: 'UnresolvedDomainEntity',
+      property: 'UnresolvedDomain',
+    },
+  },
+  {
+    name: 'Validate unresolved category',
+    schemaName: 'unresolvedCategoryValidation',
+    error: {
+      type: 'unresolvedCategory',
+      source: 'UnresolvedCategoryEntity',
+      property: 'UnresolvedCategory',
+    },
+  },
+  {
+    name: 'Validate link to \'Log\' category',
+    schemaName: 'linkToLogValidation',
+    error: {
+      type: 'linkToLog',
+      source: 'LinkToLogEntity',
+      property: 'LinkToLog',
+    },
+  },
+  {
+    name: 'Validate illegal link to \'Local\'',
+    schemaName: 'illegalLinkToLocalValidation',
+    error: {
+      type: 'illegalLinkToLocal',
+      source: 'IllegalLinkToLocalEntity',
+      property: 'IllegalLinkToLocal',
+    },
+  },
+  {
+    name: 'Validate valid schemas',
+    schemaName: 'validSchemasValidation',
+  },
+];
+
+testConfigs.forEach(test);
