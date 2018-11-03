3c813a17c4a49e3ddf745136ce2cb43eb2a09759
Denys Otrishko <shishugi@gmail.com>
2018-11-03 15:00:20 +0200

    Add convert.toJs to convert fs schema file to JS module
    PR-URL: https://github.com/metarhia/metaschema/pull/147

--- metaschema.js
+++ metaschema.js
@@ -6,6 +6,7 @@ const types = require('./lib/types');
 const decorators = require('./lib/decorators');
 const { create } = require('./lib/schema');
 const schemaFs = require('./lib/schema-fs-loader');
+const { toJs } = require('./lib/schema-converters');
 
 module.exports = Object.assign(
   {},
@@ -16,5 +17,8 @@ module.exports = Object.assign(
     fs: schemaFs,
     create,
     decorators: decorators.all,
+    convert: {
+      toJs,
+    },
   }
 );
---
+++ test/converters.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const metatests = require('metatests');
+const { convert: { toJs } } = require('../metaschema');
+
+const simpleSchema = `
+{
+  FirstName: { domain: 'Nomen', required: true },
+  MiddleName: { domain: 'Nomen' },
+}
+`;
+
+const decoratedSchema = `
+{
+  FullName: Include('FullName'),
+  Sex: { domain: 'Sex' },
+  Languages: Many('Language'),
+}
+`;
+
+const allDecoratedSchema = `
+Local({
+  FullName: Include('FullName'),
+  Sex: { domain: 'Sex' },
+  Languages: Many('Language'),
+})
+`;
+
+metatests.testSync('toJs test', test => {
+  test.testSync('must convert simple schema', t => {
+    const expected = `module.exports = ${simpleSchema};`;
+    t.strictSame(toJs(simpleSchema), expected);
+  });
+
+  test.testSync('must convert simple schema (esm)', t => {
+    const expected = `export default ${simpleSchema};`;
+    t.strictSame(toJs(simpleSchema, { useEsm: true }), expected);
+  });
+
+  test.testSync('must convert decorated schema', t => {
+    const expected =
+    `const { decorators } = require('metaschema');
+const { Many, Include } = decorators;
+
+module.exports = ${decoratedSchema};`;
+    t.strictSame(toJs(decoratedSchema), expected);
+  });
+
+  test.testSync('must convert decorated schema (esm)', t => {
+    const expected =
+    `import { decorators } from 'metaschema';
+const { Many, Include } = decorators;
+
+export default ${decoratedSchema};`;
+    t.strictSame(toJs(decoratedSchema, { useEsm: true }), expected);
+  });
+
+  test.testSync('must convert all decorated schema', t => {
+    const expected =
+    `const { decorators } = require('metaschema');
+const { Local, Many, Include } = decorators;
+
+module.exports = ${allDecoratedSchema};`;
+    t.strictSame(toJs(allDecoratedSchema), expected);
+  });
+});
