ed310873b3c78464405634720c52280ee8d288e3
Denys Otrishko <shishugi@gmail.com>
2018-12-11 16:30:52 +0200

    Refactor Action implementation
    * make Action an object with properties
    * add Execute decorator to denote Action reference
    * add Type, TransactionMode, ProcessMode, RefreshMode props
    * allow to specify custom Form name via Form field
    
    PR-URL: https://github.com/metarhia/metaschema/pull/175
    
    Co-authored-by: Timur Shemsedinov <timur.shemsedinov@gmail.com>

--- test/schema-actions.js
+++ test/schema-actions.js
@@ -6,19 +6,106 @@ const metaschema = require('..');
 const { getSchemaDir } = require('./utils');
 
 metatests.test('must load Actions', test => {
+
+  const testMethod = (test, method, ctx, expected) => {
+    method({}, ctx, {}, (err, actual) => {
+      test.error(err);
+      test.strictSame(actual, expected);
+      test.end();
+    });
+  };
+
+  const testExecute = (test, def, ctx, expected) => {
+    testMethod(test, def.Execute, ctx, expected);
+  };
+
   metaschema.fs.loadAndCreate(getSchemaDir('actions'), null, (error, ms) => {
     test.error(error);
 
-    test.equal(ms.actions.get('SchemaWithoutActions').size, 0);
-    test.equal(ms.actions.get('SchemaWithActions').size, 1);
+    test.strictSame(ms.actions.get('SchemaWithoutActions').size, 0);
+    test.strictSame(ms.actions.get('SchemaWithActions').size, 1);
 
-    ms.actions
-      .get('SchemaWithActions')
-      .get('Act')
-      .definition({ Id: 42 }, (error, id) => {
-        test.error(error);
-        test.equal(id, 'Resource42');
-        test.end();
+    test.endAfterSubtests();
+
+    test.test('SchemaWithActions test', test => {
+      const action = ms.actions
+        .get('SchemaWithActions')
+        .get('Act');
+      test.endAfterSubtests();
+      testExecute(test.test(), action.definition, { Id: 42 }, 'Resource42');
+    });
+
+    test.test('CustomActions test', test => {
+      const action = ms.actions
+        .get('CustomActions')
+        .get('Act');
+      test.strictSame(
+        action.form,
+        ms.forms.get('CustomActions.CustomForm')
+      );
+      test.endAfterSubtests();
+      testExecute(test.test(), action.definition, { Id: 13 }, 'Resource13');
+    });
+
+    test.test('ActionsExecute test', test => {
+      test.endAfterSubtests();
+
+      const actAction = ms.actions
+        .get('ActionsExecute')
+        .get('Act');
+      test.assertNot(actAction.form);
+      testExecute(
+        test.test(), actAction.definition, { Id: 42 }, { Action: 'M1' }
+      );
+      testExecute(
+        test.test(), actAction.definition, { Id: 1 }, { Action: 'M2' }
+      );
+
+      const m1Action = ms.actions
+        .get('ActionsExecute')
+        .get('M1');
+      test.strictSame(
+        m1Action.form,
+        ms.forms.get('ActionsExecute.M1')
+      );
+      testExecute(test.test(), m1Action.definition, { Id: 42 }, 'M1Resource42');
+
+      const m2Action = ms.actions
+        .get('ActionsExecute')
+        .get('M2');
+      test.strictSame(
+        m2Action.form,
+        ms.forms.get('ActionsExecute.CustomForm')
+      );
+      testExecute(test.test(), m2Action.definition, { Id: 42 }, 'M2Resource42');
+    });
+
+    test.test('ActionsExecuteForm test', test => {
+      test.endAfterSubtests();
+
+      const actAction = ms.actions
+        .get('ActionsExecuteForm')
+        .get('Act');
+      test.assertNot(actAction.form);
+      const { Execute: execute } = actAction.definition;
+
+      const t1 = test.test();
+      execute({}, { Id: 42 }, {}, (err, act) => {
+        t1.error(err);
+        t1.type(act, 'Execute');
+        t1.strictSame(act.Action, 'M1');
+        t1.strictSame(act.Form, 'CustomForm');
+        t1.end();
       });
+
+      const t2 = test.test();
+      execute({}, { Id: 13 }, {}, (err, act) => {
+        t2.error(err);
+        t2.type(act, 'Execute');
+        t2.strictSame(act.Action, 'M2');
+        t2.strictSame(act.Form, 'CustomForm');
+        t2.end();
+      });
+    });
   });
 });
