f99eef938c348a0ebd83514a0b4671a2051476c7
Denys Otrishko <shishugi@gmail.com>
2018-12-11 16:30:52 +0200

    Refactor Action implementation
    * make Action an object with properties
    * add type, transactionMode, processMode, refreshMode props
    * allow to specify custom Form name via Form field

--- test/schema-actions.js
+++ test/schema-actions.js
@@ -6,19 +6,71 @@ const metaschema = require('..');
 const { getSchemaDir } = require('./utils');
 
 metatests.test('must load Actions', test => {
+
+  const testMethod = (test, method, ctx, expected) => {
+    method({}, ctx, (err, actual) => {
+      test.error(err);
+      test.strictSame(actual, expected);
+      test.end();
+    });
+  };
+
+  const testExecute = (test, def, ctx, expected) => {
+    testMethod(test, def.Execute, ctx, expected);
+  };
+
   metaschema.fs.loadAndCreate(getSchemaDir('actions'), null, (error, ms) => {
     test.error(error);
 
-    test.equal(ms.actions.get('SchemaWithoutActions').size, 0);
-    test.equal(ms.actions.get('SchemaWithActions').size, 1);
+    test.strictSame(ms.actions.get('SchemaWithoutActions').size, 0);
+    test.strictSame(ms.actions.get('SchemaWithActions').size, 1);
 
-    ms.actions
-      .get('SchemaWithActions')
-      .get('Act')
-      .definition({ Id: 42 }, (error, id) => {
-        test.error(error);
-        test.equal(id, 'Resource42');
-        test.end();
-      });
+    test.endAfterSubtests();
+
+    test.test('SchemaWithActions test', test => {
+      const action = ms.actions
+        .get('SchemaWithActions')
+        .get('Act');
+      test.endAfterSubtests();
+      testExecute(test.test(), action.definition, { Id: 42 }, 'Resource42');
+    });
+
+    test.test('CustomActions test', test => {
+      const action = ms.actions
+        .get('CustomActions')
+        .get('Act');
+      test.strictSame(
+        action.forms.Act,
+        ms.forms.get('CustomActions.CustomForm')
+      );
+      test.endAfterSubtests();
+      testExecute(test.test(), action.definition, { Id: 13 }, 'Resource13');
+    });
+
+    test.testSync('CustomActionsMethods test', test => {
+      const action = ms.actions
+        .get('CustomActionsMethods')
+        .get('Act');
+
+      test.assertNot(action.definition.Execute);
+
+      test.strictSame(
+        action.forms.M1,
+        ms.forms.get('CustomActionsMethods.M1')
+      );
+      test.strictSame(
+        action.forms.M2,
+        ms.forms.get('CustomActionsMethods.CustomForm')
+      );
+
+      test.endAfterSubtests();
+
+      const { getMethod, Methods } = action.definition;
+      testMethod(test.test(), getMethod, { Id: 42 }, 'M1');
+      testMethod(test.test(), getMethod, { Id: 1 }, 'M2');
+
+      testExecute(test.test(), Methods.M1, { Id: 13 }, 'M1Resource13');
+      testExecute(test.test(), Methods.M2, { Id: 13 }, 'M2Resource13');
+    });
   });
 });
