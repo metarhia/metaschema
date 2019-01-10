9288ae20917e26299374b5d96ae9e693b184faab
Denys Otrishko <shishugi@gmail.com>
2019-01-10 17:46:10 +0200

    Expose processSchema to allow loading of 'string' schemas
    PR-URL: https://github.com/metarhia/metaschema/pull/189

--- metaschema.js
+++ metaschema.js
@@ -2,10 +2,12 @@
 
 const decorators = require('./lib/decorators');
 const { create, extractDecorator } = require('./lib/schema');
+const { processSchema } = require('./lib/schema-loader');
 const schemaFs = require('./lib/schema-fs-loader');
 
 module.exports = {
   fs: schemaFs,
+  processSchema,
   create,
   decorators: decorators.all,
   extractDecorator,
