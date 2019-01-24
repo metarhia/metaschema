81fc7d40ebcc0ee42d79cf3e692434a0b425d38d
Dmytro Nechai <nechaido@gmail.com>
2019-01-24 14:43:52 +0200

    Treat datasets in DisplayModes as optional
    PR-URL: https://github.com/metarhia/metaschema/pull/201

--- test/display-modes.js
+++ test/display-modes.js
@@ -13,7 +13,7 @@ const {
 const path = getSchemaDir('displayModes');
 
 metatests.test('must properly load displayModes', test => {
-  metaschema.fs.loadAndCreate(path, null, error => {
+  metaschema.fs.loadAndCreate(path, null, (error, ms) => {
     const expected = new MetaschemaError([
       new SchemaValidationError(
         'unresolved',
@@ -27,6 +27,10 @@ metatests.test('must properly load displayModes', test => {
     removeStack(error.errors);
     removeStack(expected.errors);
     test.strictSame(error.errors, expected.errors);
+
+    const category = ms.categories.get('Category');
+    test.assert(category.displayModes.has('Display'));
+    test.assert(category.displayModes.has('NoDatasetsMode'));
     test.end();
   });
 });
