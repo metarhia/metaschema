2733dadab5e9bb8cc4b0ed224dd847ea33ca8c5c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-02 17:58:19 +0200

    New metaschema implementation
    PR-URL: https://github.com/metarhia/metaschema/pull/247

--- metaschema.js
+++ metaschema.js
@@ -1,18 +1,7 @@
 'use strict';
 
-const { Metaschema } = require('./lib/schema');
-const { extractDecorator, extractByPath } = require('./lib/utils');
-const { processSchema } = require('./lib/schema-processor');
-const fs = require('./lib/fs-loader');
-const defaultConfig = require('./lib/config');
-const errors = require('./lib/errors');
+const { Schema } = require('./lib/schema.js');
 
 module.exports = {
-  Metaschema,
-  fs,
-  default: defaultConfig,
-  processSchema,
-  extractDecorator,
-  extractByPath,
-  errors,
+  Schema,
 };
