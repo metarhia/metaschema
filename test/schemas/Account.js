238f14342c5d7528b8434ef21fe98886730a4eb4
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-05-14 23:17:33 +0300

    Remove references to unknown schemas
    PR-URL: https://github.com/metarhia/metaschema/pull/315

--- test/schemas/Account.js
+++ test/schemas/Account.js
@@ -8,22 +8,13 @@
   roles: { many: 'Role' },
 
   fullName: {
-    givenName: { type: 'string', required: false },
-    middleName: { type: 'string', required: false },
+    given: { type: 'string', required: false },
+    middle: { type: 'string', required: false },
     surname: { type: 'string', required: false },
   },
 
   birth: {
-    birthDate: { type: 'string', required: false },
-    birthPlace: { type: 'string', required: false },
-  },
-
-  address: {
-    country: { type: 'Country', required: false },
-    province: { type: 'Province', required: false },
-    city: { type: 'City', required: false },
-    address1: { type: 'string', required: false },
-    address2: { type: 'string', required: false },
-    zipCode: { type: 'string', required: false },
+    date: { type: 'string', required: false },
+    place: { type: 'string', required: false },
   },
 });
