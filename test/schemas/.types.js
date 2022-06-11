2db08acf6bf12474b720ea963b33d953c63ca362
georgolden <georgoldenb@gmail.com>
2022-06-11 02:38:34 +0200

    Better naming, better methods order

--- test/schemas/.types.js
+++ test/schemas/.types.js
@@ -8,6 +8,9 @@
     kind: 'scalar',
     rules: ['length'],
     symbols: '1234567890e.',
+
+    construct() {},
+
     checkType(src, path) {
       if (typeof src !== 'string') {
         return `Field "${path}" not a decimal 1`;
@@ -21,6 +24,5 @@
           return `Field "${path}" not a decimal 3`;
       }
     },
-    construct() {},
   },
 });
