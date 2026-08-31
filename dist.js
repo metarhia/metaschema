12c014de7a4310df3b417677e47653c6293d2ca4
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2026-09-01 01:26:24 +0300

    Apply metaskills: js-style

--- dist.js
+++ dist.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const { constants } = require('./lib/kinds.js');
+const { constants, getKindMetadata } = require('./lib/kinds.js');
 const schema = require('./lib/schema.js');
 const model = require('./lib/model.js');
 
 module.exports = {
   ...constants,
+  getKindMetadata,
   ...schema,
   ...model,
 };
